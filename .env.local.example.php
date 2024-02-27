a04d611a9b52b00464971ee5cb0b672902b0e3ee
v-gama <victor.p.gama@gmail.com>
2024-02-27 18:11:21 -0300

    env example adicionado

---
+++ .env.local.example.php
@@ -0,0 +1,11 @@
+
+<?php
+
+return array(
+    'db_host' => '127.0.0.1',
+    'db_port' => 5432,
+    'db_name' => 'nome_banco_local', # nome do seu banco de dados local
+    'db_connection' => 'pgsql',
+    'db_user' => 'nome_usuario_local', # nome do seu usuario local
+    'db_pass' => 'senha_banco_local' # senha do seu usuario local
+);
