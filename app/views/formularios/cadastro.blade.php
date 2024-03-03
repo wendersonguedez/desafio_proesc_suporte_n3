084572e73435cb5791387118275dc1147093714d
wenderson <wendersonguedes6@gmail.com>
2024-03-02 23:09:18 -0300

    Implementação de mensagens de erros para validação de cadastro de pessoas

--- app/views/formularios/cadastro.blade.php
+++ app/views/formularios/cadastro.blade.php
@@ -11,27 +11,31 @@
                 {{ Session::get('success') }}
             </div>
         @endif
-        
+
         {{ Form::open(['url' => 'cadastrar-pessoa', 'method' => 'post']) }}
 
         <div class="form-group">
             {{ Form::label('nome', 'Nome') }}
             {{ Form::text('nome', null, ['class' => 'form-control', 'placeholder' => 'Digite seu nome']) }}
+            {{ $errors->first('nome', '<span class="text-danger">:message</span>') }}
         </div>
 
         <div class="form-group">
             {{ Form::label('email', 'Email') }}
             {{ Form::email('email', null, ['class' => 'form-control', 'placeholder' => 'Digite seu email']) }}
+            {{ $errors->first('email', '<span class="text-danger">:message</span>') }}
         </div>
 
         <div class="form-group">
             {{ Form::label('cpf', 'CPF') }}
             {{ Form::text('cpf', null, ['class' => 'form-control', 'placeholder' => 'Digite seu CPF']) }}
+            {{ $errors->first('cpf', '<span class="text-danger">:message</span>') }}
         </div>
 
         <div class="form-group">
             {{ Form::label('telefone', 'Telefone') }}
             {{ Form::text('telefone', null, ['class' => 'form-control', 'placeholder' => 'Digite seu telefone']) }}
+            {{ $errors->first('telefone', '<span class="text-danger">:message</span>') }}
         </div>
 
         <div class="form-group">
@@ -39,5 +43,9 @@
         </div>
 
         {{ Form::close() }}
+
+        <div class="form-group">
+            <a href="{{ URL::to('formulario-excel') }}" class="btn btn-primary">Adicionar via Arquivo CSV</a>
+        </div>
     </div>
 @endsection
