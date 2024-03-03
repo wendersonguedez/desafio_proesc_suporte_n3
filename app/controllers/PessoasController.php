7934e3805c3c8379f6039eee62f98b0f62c3d643
wenderson <wendersonguedes6@gmail.com>
2024-03-02 23:07:37 -0300

    Implementação dos novos métodos

--- app/controllers/PessoasController.php
+++ app/controllers/PessoasController.php
@@ -1,13 +1,102 @@
 <?php
 
-class PessoasController extends BaseController {
+use Illuminate\Support\Facades\Input;
+use Symfony\Component\HttpFoundation\File;
 
-    public function visualizarFormulario() {
+class PessoasController extends BaseController
+{
+
+    public function visualizarFormulario()
+    {
         return View::make('formularios.cadastro');
     }
 
-    public function cadastrarPessoa() {
+    public function cadastrarPessoa()
+    {
+        $rules = array(
+            'nome' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'cpf' => 'required|unique:pessoas',
+            'telefone' => 'required|max:20'
+        );
+
+        $messages = array(
+            'nome.required' => 'O campo Nome é obrigatório.',
+            'email.required' => 'O campo Email é obrigatório.',
+            'email.email' => 'Por favor, insira um endereço de email válido.',
+            'cpf.required' => 'O campo CPF é obrigatório.',
+            'cpf.unique' => 'O CPF já está em uso.',
+            'telefone.required' => 'O campo Telefone é obrigatório.',
+        );
+
+        $validator = Validator::make(Input::all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            return Redirect::back()->withErrors($validator)->withInput();
+        } else {
+            $pessoa = new Pessoa;
+            $pessoa->nome = Input::get('nome');
+            $pessoa->email = Input::get('email');
+            $pessoa->cpf = Input::get('cpf');
+            $pessoa->telefone = Input::get('telefone');
+            $pessoa->grupo_id = 2;
+            $pessoa->save();
+        }
 
         return Redirect::to('/cadastro')->with('success', 'Cadastro realizado com sucesso!');
+    }
+
+    public function visualizarFormularioExcel()
+    {
+        return View::make('formularios.cadastro-excel');
+    }
+
+    public function inserirUsuariosDoArquivoExcel()
+    {
+        $firstLine = true;
+
+        $grupos = [
+            "RESPONSAVEL" => 1,
+            "ESTUDANTE" => 2,
+            "DOCENTE" => 3
+        ];
+
+        $file = $_FILES['excel'];
+
+        $dataFile = fopen($file['tmp_name'], 'r');
+
+        while ($row = fgetcsv($dataFile, 1000, ",")) {
+            if (!empty($row)) {
+
+                if ($firstLine) {
+                    $firstLine = false;
+                    continue;
+                }
+
+                $nome = trim($row[0]);
+                $email = trim($row[1]);
+                $cpf = trim($row[2]);
+                $telefone = trim($row[3]);
+                $grupo_tipo = trim($row[4]);
+
+                // Substituir o valor do tipo de grupo pelo ID correspondente usando array de mapeamento
+                if (isset($grupos[$grupo_tipo])) {
+                    $grupo_id = $grupos[$grupo_tipo];
+                } else {
+                    return;
+                }
+
+                $query = Pessoa::insert(
+                    array(
+                        'nome' => $nome,
+                        'email' => $email,
+                        'cpf' => $cpf,
+                        'telefone' => $telefone,
+                        'grupo_id' => $grupo_id
+                    )
+                );
+            }
+            return Redirect::back()->with('success', 'Cadastro realizado com sucesso!');
+        }
     }
 }
--- app/services/NotasFormatar.php
+++ app/services/NotasFormatar.php
@@ -2,7 +2,8 @@
 
 namespace App\Services;
 
-Class NotasFormatar {
+class NotasFormatar
+{
     public function formataNotasPeriodos($notas, $criterio_avaliativo)
     {
         if (!empty($notas) && !empty($criterio_avaliativo)) {
@@ -14,7 +15,7 @@ Class NotasFormatar {
         return $notas;
     }
 
-    public function calculaNotaFinal($notas, $disciplinas, $criterio_avaliativo)
+    /*public function calculaNotaFinal($notas, $disciplinas, $criterio_avaliativo)
     {
         if (!empty($notas) && !empty($disciplinas) && !empty($criterio_avaliativo)) {
             $array_notas = [];
@@ -32,14 +33,75 @@ Class NotasFormatar {
 
             return $this->{"calculo$criterio_avaliativo->calculo_id"}($array_notas, $criterio_avaliativo->arredondamento_id);
         }
+    }*/
+
+    public function calculaNotaFinal($notas, $disciplinas, $criterio_avaliativo)
+    {
+        if (!empty($notas) && !empty($disciplinas) && !empty($criterio_avaliativo)) {
+            $array_notas = [];
+
+            foreach ($disciplinas as $disciplina) {
+                $nota_1bim = 0;
+                $nota_2bim = 0;
+                $nota_3bim = 0;
+                $nota_4bim = 0;
+
+                foreach ($notas as $nota) {
+                    if ($nota->disciplina_id == $disciplina->disciplina_id) {
+                        switch ($nota->periodo_id) {
+                            case 1:
+                                $nota_1bim = $nota->valor_nota;
+                                break;
+                            case 2:
+                                $nota_2bim = $nota->valor_nota;
+                                break;
+                            case 3:
+                                $nota_3bim = $nota->valor_nota;
+                                break;
+                            case 4:
+                                $nota_4bim = $nota->valor_nota;
+                                break;
+                        }
+                    }
+                }
+
+                // Calcula a nota final para a disciplina atual
+                $nota_final = ($nota_1bim + $nota_2bim + ($nota_3bim * 2) + ($nota_4bim * 2)) / 6;
+
+                $notaFinalArredondada = $this->arredondamento($nota_final);
+
+                $array_notas[] = [
+                    'valor_nota' => $notaFinalArredondada,
+                    'disciplina_id' => $disciplina->disciplina_id
+                ];
+            }
+
+            return $array_notas;
+        }
     }
 
-    function calculo1($array_notas, $arredondamento_id) {
+    public function arredondamento($nota)
+    {
+        $fracao = explode('.', $nota);
+        $decimal = isset($fracao[1]) ? $fracao[1][0] : '';
+
+        if ($decimal >= 7) {
+            $notaArredondada = round($nota, 1);
+        } else {
+            $notaArredondada = $nota;
+        }
+
+        return number_format($notaArredondada, 2);
+    }
+
+
+    function calculo1($array_notas, $arredondamento_id)
+    {
         $array_notas_finais = [];
-    
+
         foreach ($array_notas as $nota) {
             $disciplina_id = $nota['disciplina_id'];
-    
+
             if (!isset($array_notas_finais[$disciplina_id])) {
                 $array_notas_finais[$disciplina_id] = [
                     'disciplina_id' => $disciplina_id,
@@ -47,38 +109,39 @@ Class NotasFormatar {
                     'contagem' => 0
                 ];
             }
-    
+
             $array_notas_finais[$disciplina_id]['soma'] += $nota['valor_nota'];
             $array_notas_finais[$disciplina_id]['contagem']++;
         }
-    
+
         foreach ($array_notas_finais as &$nota_final) {
             $nota_final['valor_nota'] = $this->arredondaNota($nota_final['soma'] / $nota_final['contagem'], $arredondamento_id);
         }
-    
+
         return $array_notas_finais;
     }
 
-    function calculo2($array_notas, $arredondamento_id) {
+    function calculo2($array_notas, $arredondamento_id)
+    {
         $array_notas_finais = [];
-    
+
         foreach ($array_notas as $nota) {
             $disciplina_id = $nota['disciplina_id'];
-    
+
             if (!isset($array_notas_finais[$disciplina_id])) {
                 $array_notas_finais[$disciplina_id] = [
                     'disciplina_id' => $disciplina_id,
                     'soma' => 0
                 ];
             }
-    
+
             $array_notas_finais[$disciplina_id]['soma'] += $nota['valor_nota'];
         }
-    
+
         foreach ($array_notas_finais as &$nota_final) {
             $nota_final['valor_nota'] = $this->arredondaNota($nota_final['soma'], $arredondamento_id);
         }
-    
+
         return $array_notas_finais;
     }
 
@@ -94,14 +157,29 @@ Class NotasFormatar {
     protected function arredondamento1($valor_nota)
     {
         $valor_nota_arredondada = ceil($valor_nota);
-        
+
         return $valor_nota_arredondada;
     }
 
     protected function arredondamento2($valor_nota)
     {
         $valor_nota_arredondada = floor($valor_nota);
-        
+
         return $valor_nota_arredondada;
     }
+
+    public function novoCalculoNotaFinal($notas)
+    {
+        if (!empty($notas)) {
+            $notas_finais = [];
+
+            foreach ($notas as $nota) {
+                if ($nota->periodo_id = 1) {
+                    $notas_finais[] = $nota->valor_nota;
+                }
+            }
+        }
+        return $notas;
+    }
+
 }
