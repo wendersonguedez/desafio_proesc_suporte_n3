5b7b1ed08864f80c18b53b56f958e8fe1f34e26b
wenderson <wendersonguedes6@gmail.com>
2024-03-02 23:10:31 -0300

    Mudança na estrutura do boletim, permitindo visualizar as notas vermelhas, de acordo com criterio estabelecido

--- app/views/relatorios/boletim.blade.php
+++ app/views/relatorios/boletim.blade.php
@@ -10,10 +10,10 @@
             <div class="col-md-12">
                 <table class="table table-bordered mb-4">
                     <tbody>
-                        <tr>
-                            <th scope="row" class="text-left">ALUNO: {{ $aluno->nome }}</th>
-                            <td class="text-left">TURMA: {{ $aluno->turma_nome }}</td>
-                        </tr>
+                    <tr>
+                        <th scope="row" class="text-left">ALUNO: {{ $aluno->nome }}</th>
+                        <td class="text-left">TURMA: {{ $aluno->turma_nome }}</td>
+                    </tr>
                     </tbody>
                 </table>
             </div>
@@ -24,37 +24,43 @@
                 <h4 class="text-center mb-3">NOTAS</h4>
                 <table class="table table-bordered">
                     <thead>
-                        <tr>
-                            <th scope="col">DISCIPLINAS</th>
-                            @foreach ($diarios as $diario)
-                                <th scope="col"> {{ $diario->periodo_nome }} </th>
-                            @endforeach
-                                <th scope="col"> {{ 'NOTA FINAL' }} </th>
-                        </tr>
+                    <tr>
+                        <th scope="col">DISCIPLINAS</th>
+                        @foreach ($diarios as $diario)
+                            <th scope="col"> {{ $diario->periodo_nome }} </th>
+                        @endforeach
+                        <th scope="col"> {{ 'NOTA FINAL' }} </th>
+                    </tr>
                     </thead>
                     <tbody>
-                        @foreach ($disciplinas as $disciplina)
-                            <tr>
-                                <th scope="row"> {{$disciplina->disciplina_nome}} </th>
-                                    @foreach ($avaliacoes as $avaliacao)
-                                        @if ($avaliacao->disciplina_id == $disciplina->disciplina_id)
-                                            @foreach ($notas_periodos as $nota_periodo)
-                                                @if ($nota_periodo->avaliacao_id == $avaliacao->avaliacao_id && $nota_periodo->disciplina_id == $avaliacao->disciplina_id)
-                                                    <td> {{ $nota_periodo->valor_nota }} </td>
-                                                @endif
-                                            @endforeach
+                    @foreach ($disciplinas as $disciplina)
+                        <tr>
+                            <th scope="row"> {{$disciplina->disciplina_nome}} </th>
+                            @foreach ($avaliacoes as $avaliacao)
+                                @if ($avaliacao->disciplina_id == $disciplina->disciplina_id)
+                                    @foreach ($notas_periodos as $nota_periodo)
+                                        @if ($nota_periodo->avaliacao_id == $avaliacao->avaliacao_id && $nota_periodo->disciplina_id == $avaliacao->disciplina_id)
+                                            <td class="{{ $nota_periodo->valor_nota < 70 ? 'text-danger' : '' }}"> {{ $nota_periodo->valor_nota }} </td>
                                         @endif
                                     @endforeach
+                                @endif
+                            @endforeach
 
-                                    @foreach ($notas_finais as $nota_final)
-                                        @if ($nota_final['disciplina_id'] == $disciplina->disciplina_id)
-                                            <td> {{$nota_final['valor_nota']}} </td>
-                                        @endif
-                                    @endforeach
-                            </tr>
-                        @endforeach
+                            @foreach ($notas_finais as $nota_final)
+                                @if ($nota_final['disciplina_id'] == $disciplina->disciplina_id)
+                                    <td class="{{ $nota_final['valor_nota'] < 70 ? 'text-danger' : '' }}"> {{$nota_final['valor_nota']}} </td>
+                                @endif
+                            @endforeach
+                        </tr>
+                    @endforeach
                     </tbody>
                 </table>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <p class="text-center">Nota Mínima: 70 | Nota Máxima: 100</p>
             </div>
         </div>
     </div>
